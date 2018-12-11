7da1073bc817aac32e26ceba52bbf234b7d2f7a9
ruccin <Hecate4428@daum,net>
2018-12-11 16:20:03 +0900

    fixed

--- test_tcp_server.c
+++ test_tcp_server.c
@@ -2,14 +2,14 @@
 #include <unistd.h>
 #include <sys/socket.h>
 #include <stdlib.h>
-#include <netint/in.h>
+#include <netinet/in.h>
 #include <string.h>
 #define PORT 8080
 
 int main(int argc, char const *argv[]){
     
     int server_fd, new_socket, valread;
-    struct sockadder_in address;
+    struct sockaddr_in address;
     int opt = 1;
     int addrlen = sizeof(address);
     char buffer[1024] = {0};
@@ -18,7 +18,7 @@ int main(int argc, char const *argv[]){
     //Creating socket file descriptor
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0){
         perror("socket failed");
-        exit(EXIT_FAULURE);
+        exit(EXIT_FAILURE);
     }
 
     //Forcefully attaching socket to the port 8080
@@ -44,7 +44,7 @@ int main(int argc, char const *argv[]){
 
     if((new_socket = accept(server_fd, (struct sockaddr *)&address, (socklen_t *)&addrlen)) < 0 ){
         perror("accept");
-        exit(EXIT_FAILURE;)
+        exit(EXIT_FAILURE);
     }
 
     valread = read(new_socket, buffer, 1024);
