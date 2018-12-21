ee784e1bab09728057373d53d1506444e5706ba2
ruccin <Hecate4428@daum,net>
2018-12-21 17:20:47 +0900

    fixed

--- filter_ver4.c
+++ filter_ver4.c
@@ -4,13 +4,6 @@
 #include <linux/netfilter.h>
 #include <linux/ip.h>
 #include <linux/tcp.h>
-#include <linux/net_namespace.h>
-
-#define NIPQUAD(addr) \
-	((unsigned char *)&addr)[0], \
-	((unsigned char *)&addr)[1], \
-	((unsigned char *)&addr)[2], \
-	((unsigned char *)&addr)[3]
 
 static struct nf_hook_ops nfho;
 
@@ -19,7 +12,8 @@ static unsigned int hook_func (void *priv,
 								const struct nf_hook_state *state) {
 
 	struct iphdr *iph = ip_hdr(skb);
-	struct ethhdr *eth = eth_hdr(skb);
+	//struct ethhdr *eth = eth_hdr(skb);
+	//struct icmphdr *icmh = icmp_hdr(skb);
 
 	if (!iph) {
 		
@@ -27,7 +21,13 @@ static unsigned int hook_func (void *priv,
 	}
 
 	printk("NF_IP_HOOK:\n");
-	printk("src mac %pM, dst mac %pM\n", eth->h_source, eth->h_dest);
+	//printk("src mac %pM, dst mac %pM\n", eth->h_source, eth->h_dest);
+
+	if (iph->protocol == IPPROTO_ICMP) {
+
+		printk("ICMP\n");
+
+	}
 
     return NF_ACCEPT;
 
@@ -38,7 +38,7 @@ static int my_init(void) {
 	nfho.hook = hook_func;
 	nfho.pf = PF_INET;
 	nfho.hooknum = NF_INET_PRE_ROUTING;
-	nfho.priority = 1;
+	nfho.priority = NF_IP_PRI_FIRST;
 	nf_register_net_hook (NULL, &nfho);
 
 	return 0;
