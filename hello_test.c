dfc1b4ba8ea056fd598a6bf51efa05b5b82e7557
ruccin <Hecate4428@daum,net>
2018-12-23 18:49:19 +0900

    fixed

--- hello_test.c
+++ hello_test.c
@@ -6,7 +6,6 @@
 #include <linux/skbuff.h>
 
 static struct nf_hook_ops nfho;
-static struct net hook_net;
 
 static unsigned int hook_func (void *priv,
                         struct sk_buff *skb,
@@ -25,13 +24,13 @@ static int my_init(void) {
     nfho.priority = NF_IP_PRI_CONNTRACK_CONFIRM - 1;
 	nfho.priv = NULL;
 
-    nf_register_net_hook(&hook_net, &nfho);
+    nf_register_net_hook(&init_net, &nfho);
 
     return 0;
 }
 
 static void my_exit(void) {
-    nf_unregister_net_hook(&hook_net, &nfho);
+    nf_unregister_net_hook(&init_net, &nfho);
 }
 
 module_init(my_init);
