f5053d1f65c811b7725eb1fdef8c0f00713482b8
ruccin <Hecate4428@daum,net>
2018-12-24 15:11:25 +0900

    fixed

--- hello_rohc.c
+++ hello_rohc.c
@@ -28,6 +28,11 @@ static unsigned int hook_func (void *priv,
     
     struct iphdr *iph;
 	struct tcphdr *tph;
+
+	unsigned char ip_buffer[BUFFER_SIZE];
+	struct rohc_buf ip_packet = rohc_buf_init_empty(ip_buffer, BUFFER_SIZE);
+	struct iphdr *rohc_iph;
+	size_t i;
 	
 	iph = ip_hdr(skb);
 	tph = tcp_hdr(skb);
@@ -38,52 +43,37 @@ static unsigned int hook_func (void *priv,
 	if (iph->protocol == IPPROTO_TCP) {
 		pr_info("Hello, TCP\n");
 
-		test();
-//		rohc_comp(skb);
+		pr_info("Test Fake Packet\n");
 
-	}
+		rohc_iph = (struct iphdr *) rohc_buf_data(ip_packet);
+		rohc_iph->version = 4;
+		rohc_iph->ihl = 5;
+		ip_packet.len += rohc_iph->ihl * 4;
+		rohc_iph->tos = 0;
+		rohc_iph->tot_len = htons(ip_packet.len + strlen(FAKE_PAYLOAD));
+		rohc_iph->id = 0;
+		rohc_iph->frag_off = 0;
+		rohc_iph->ttl = 1;
+		rohc_iph->protocol = 134;
+		rohc_iph->check = 0x3fa9;
+		rohc_iph->saddr = htonl(0x01020304);
+		rohc_iph->daddr = htonl(0x05060708);
 
-    return NF_ACCEPT;
-}
+		rohc_buf_append(&ip_packet, (uint8_t *)FAKE_PAYLOAD, strlen(FAKE_PAYLOAD));
 
-static int test(void) {
-
-	unsigned char ip_buffer[BUFFER_SIZE];
-	struct rohc_buf ip_packet = rohc_buf_init_empty(ip_buffer, BUFFER_SIZE);
-	struct iphdr *rohc_iph;
-	size_t i;
-
-	pr_info("Test Fake Packet\n");
-
-	rohc_iph = (struct iphdr *) rohc_buf_data(ip_packet);
-	rohc_iph->version = 4;
-	rohc_iph->ihl = 5;
-	ip_packet.len += rohc_iph->ihl * 4;
-	rohc_iph->tos = 0;
-	rohc_iph->tot_len = htons(ip_packet.len + strlen(FAKE_PAYLOAD));
-	rohc_iph->id = 0;
-	rohc_iph->frag_off = 0;
-	rohc_iph->ttl = 1;
-	rohc_iph->protocol = 134;
-	rohc_iph->check = 0x3fa9;
-	rohc_iph->saddr = htonl(0x01020304);
-	rohc_iph->daddr = htonl(0x05060708);
-
-	rohc_buf_append(&ip_packet, (uint8_t *)FAKE_PAYLOAD, strlen(FAKE_PAYLOAD));
-
-	for (i=0; i<ip_packet.len; i++) {
-		pr_info("0x%02x ", rohc_buf_byte_at(ip_packet, i));
-		if (i != 0 && ((i + 1) % 8) == 0) {
+		for (i=0; i<ip_packet.len; i++) {
+			pr_info("0x%02x ", rohc_buf_byte_at(ip_packet, i));
+			if (i != 0 && ((i + 1) % 8) == 0) {
+				pr_info("\n");
+			}
+		}
+		if (i != 0 && (i % 8) != 0) {
 			pr_info("\n");
 		}
-	}
-	if (i != 0 && (i % 8) != 0) {
-		pr_info("\n");
-	}
 
-	return NF_ACCEPT;
+    else
+		return NF_ACCEPT;
 }
-
 
 /*
 static int rohc_comp(struct sk_buff *skb) {
