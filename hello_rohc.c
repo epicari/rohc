8f920b1a33f26200e7f7b11a1387b63a6e230fbb
ruccin <Hecate4428@daum,net>
2018-12-24 15:05:54 +0900

    fixed

--- hello_rohc.c
+++ hello_rohc.c
@@ -6,12 +6,12 @@
 #include <linux/skbuff.h>
 #include <linux/ip.h>
 #include <linux/tcp.h>
-#include <linux/time.h>
 #include <linux/rohc/rohc.h>
 #include <linux/rohc/rohc_comp.h>
 #include <linux/rohc/rohc_decomp.h>
 
 #define BUFFER_SIZE 10000
+#define FAKE_PAYLOAD "hello, world!"
 
 static struct nf_hook_ops nfho;
 
@@ -38,12 +38,51 @@ static unsigned int hook_func (void *priv,
 	if (iph->protocol == IPPROTO_TCP) {
 		pr_info("Hello, TCP\n");
 
-		rohc_comp(skb);
+		test();
+//		rohc_comp(skb);
+
 	}
 
     return NF_ACCEPT;
 }
 
+static int test(void) {
+
+	unsigned char ip_buffer[BUFFER_SIZE];
+	struct rohc_buf ip_packet = rohc_buf_init_empty(ip_buffer, BUFFER_SIZE);
+	struct iphdr *rohc_iph;
+
+	pr_info("Test Fake Packet\n");
+
+	rohc_iph = (struct iphdr *) rohc_buf_data(ip_packet);
+	rohc_iph->version = 4;
+	rohc_iph->ihl = 5;
+	ip_packet.len += rohc_iph->ihl * 4;
+	rohc_iph->tos = 0;
+	rohc_iph->tot_len = htons(ip_packet.len + skb->data_len);
+	rohc_iph->id = 0;
+	rohc_iph->frag_off = 0;
+	rohc_iph->ttl = 1;
+	rohc_iph->protocol = 134;
+	rohc_iph->check = 0x3fa9;
+	rohc_iph->saddr = htonl(0x01020304);
+	rohc_iph->daddr = htonl(0x05060708);
+
+	rohc_buf_append(&ip_packet, (uint8_t *)FAKE_PAYLOAD, strlen(FAKE_PAYLOAD));
+
+	for (i=0; i<ip_packet.len; i++) {
+		pr_info("0x%02x ", rohc_buf_byte_at(ip_packet, i));
+		if (i != 0 && ((i + 1) % 8) == 0) {
+			pr_info("\n");
+		}
+	}
+	if (i != 0 && (i % 8) != 0) {
+		pr_info("\n");
+	}
+}
+
+
+/*
 static int rohc_comp(struct sk_buff *skb) {
 
 	struct rohc_comp *compressor;
@@ -105,7 +144,7 @@ static struct rohc_comp * create_compressor(void) {
 
 	return compressor;
 }
-
+*/
 static int my_init(void) {
     nfho.hook = hook_func;
     nfho.hooknum = NF_INET_LOCAL_IN;
