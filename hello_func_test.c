648c3a5a89c5e272bf4f671e72c25ac25e411ed9
ruccin <Hecate4428@daum,net>
2018-12-24 16:57:53 +0900

    fixed

--- hello_func_test.c
+++ hello_func_test.c
@@ -7,8 +7,8 @@
 #include <linux/ip.h>
 #include <linux/tcp.h>
 
-#include <linux/rohc_buf.h>
-#include <linux/rohc_comp.h>
+#include <rohc/rohc_buf.h>
+#include <rohc/rohc_comp.h>
 
 static struct nf_hook_ops nfho;
 
