81f0faeead26c6c8104d7b960f03d5a5263513ab
ruccin <Hecate4428@daum,net>
2019-01-07 18:36:13 +0900

    fixed

--- kmod.c
+++ kmod.c
@@ -134,7 +134,7 @@ static int gen_false_random_num(const struct rohc_comp *const comp,
 
 static int my_init(void) {
     nfho.hook = hook_func;
-    nfho.hooknum = NF_IP_POST_ROUTING;
+    nfho.hooknum = NF_INET_POST_ROUTING;
     nfho.pf = NFPROTO_IPV4;
     nfho.priority = NF_IP_PRI_CONNTRACK_CONFIRM - 1;
 	nfho.priv = NULL;
