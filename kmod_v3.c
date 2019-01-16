1cc82393ea71ad5954e9ba649b4ddb0e0b509e83
ruccin <Hecate4428@daum,net>
2019-01-16 17:53:10 +0900

    fixed

--- kmod_v3.c
+++ kmod_v3.c
@@ -173,17 +173,17 @@ static unsigned int hook_init (void *priv,
 	struct tcphdr *tph;
 	const struct iphdr *ih;
 
+	iph = ip_hdr(skb);
+	tph = tcp_hdr(skb);
+	ih = skb_header_pointer(skb, iph->frag_off, sizeof(iph), &iph);
+
+	if (ih == NULL) {
+		pr_info("TRUNCATED\n");
+		return NF_DROP;
+	}
+
 	if (iph->protocol == IPPROTO_TCP) {
 		int i;
-		
-		iph = ip_hdr(skb);
-		tph = tcp_hdr(skb);
-		ih = skb_header_pointer(skb, iph->frag_off, sizeof(iph), &iph);
-
-		if (ih == NULL) {
-			pr_info("TRUNCATED\n");
-			return NF_DROP;
-		}
 
 		pr_info("Origin IP LEN=%u TTL=%u ID=%u DATA=%u",
 				ntohs(ih->tot_len), ih->ttl, ntohs(ih->id), skb->data);
@@ -225,10 +225,10 @@ static unsigned int hook_init (void *priv,
 			goto free_comp;
 		}
 */
-		return NF_ACCEPT;
 
+		return NF_ACCEPT;
 	}
-	
+
 	return NF_ACCEPT;
 }
 
