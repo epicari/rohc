f9ebe49b5ab320b32a87976a7a9951f893cffd31
ruccin <Hecate4428@daum,net>
2019-02-13 09:21:30 +0900

    fixed

--- kmod_v2.c
+++ kmod_v2.c
@@ -343,12 +343,24 @@ static void my_decomp_exit(void) {
     nf_unregister_net_hook(&init_net, &nfho);
 }
 
+static int my_init(void) {
+	while(10){
+		my_comp();
+		my_decomp();
+	}
+	return 0;
+}
+
+static int my_exit(void) {
+	my_comp_exit();
+	my_decomp_exit();
+}
 /* Test module... select one */
 
 module_init(my_comp);
-module_init(my_decomp);
+//module_init(my_decomp);
 module_exit(my_comp_exit);
-module_exit(my_decomp_exit);
+//module_exit(my_decomp_exit);
 
 MODULE_VERSION(PACKAGE_VERSION PACKAGE_REVNO);
 MODULE_LICENSE("GPL");
