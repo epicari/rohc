409840953f792980e96cbabc0f70957241e33d8e
epicari <aiek261247@gmail.com>
2019-03-22 05:49:07 +0900

    test

--- kmod.c
+++ kmod.c
@@ -215,7 +215,8 @@ static int rohc_comp(struct rohc_init *rcouple,
 		.nsec = unix_ts.tv_nsec
 	};
 	//size_t output_pkt_max_len = TCP_IP_HDR_LEN + BUFFER_SIZE;
-	struct rohc_buf rohc_packet = rohc_buf_init_empty(rcouple->rohc_packet_out,
+	uint8_t rohc_buff[BUFFER_SIZE];
+	struct rohc_buf rohc_packet = rohc_buf_init_empty(rohc_buff,
 													 BUFFER_SIZE);
 	struct rohc_buf ip_packet = rohc_buf_init_full(skb->data, skb->hdr_len, arrival_time);
 
