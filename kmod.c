3d119404354b2a5d42aaef3e3b689c9b8b3e8f96
epicari <aiek261247@gmail.com>
2019-04-04 23:51:03 +0900

    test

--- kmod.c
+++ kmod.c
@@ -223,14 +223,14 @@ static int rohc_comp(struct rohc_init *rcouple, struct sk_buff *skb) {
 		.nsec = unix_ts.tv_nsec
 	};
 
-	unsigned char *packet = skb->transport_header + skb->network_header;
+	uint16_t *header = skb->transport_header + skb->network_header;
 
-	struct rohc_buf ip_packet = rohc_buf_init_full(packet, skb->hdr_len, arrival_time);
+	struct rohc_buf ip_packet = rohc_buf_init_full(header, skb->hdr_len, arrival_time);
 /*
 	uint8_t rohc_pkt_buf[BUFFER_SIZE];
 	struct rohc_buf rohc_packet = rohc_buf_init_empty(rohc_pkt_buf, BUFFER_SIZE);
 */
-	struct rohc_buf rohc_packet = rohc_buf_init_empty(packet, BUFFER_SIZE);
+	struct rohc_buf rohc_packet = rohc_buf_init_empty(header, BUFFER_SIZE);
 
 	struct rohc_buf feedback_to_send = rohc_buf_init_empty(rcouple->feedback_to_send_buf, BUFFER_SIZE);
 
@@ -286,14 +286,14 @@ static int rohc_decomp(struct rohc_init *rcouple, struct sk_buff *skb) {
 		.nsec = unix_ts.tv_nsec
 	};
 
-	unsigned char *packet = skb->transport_header + skb->network_header;
+	uint16_t *header = skb->transport_header + skb->network_header;
 
-	struct rohc_buf ip_packet = rohc_buf_init_full(packet, skb->hdr_len, arrival_time);
+	struct rohc_buf ip_packet = rohc_buf_init_full(header, skb->hdr_len, arrival_time);
 /*	
 	uint8_t decomp_buf[BUFFER_SIZE];
 	struct rohc_buf decomp_packet = rohc_buf_init_empty(decomp_buf, BUFFER_SIZE);
 */
-	struct rohc_buf decomp_packet = rohc_buf_init_empty(packet, BUFFER_SIZE);
+	struct rohc_buf decomp_packet = rohc_buf_init_empty(header, BUFFER_SIZE);
 	uint8_t rcvd_feedback_buf[BUFFER_SIZE];
 	struct rohc_buf rcvd_feedback = rohc_buf_init_empty(rcvd_feedback_buf, BUFFER_SIZE);
 
