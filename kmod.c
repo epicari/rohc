30130c266ec924b6727c3cf6911d417e83962f5e
epicari <aiek261247@gmail.com>
2019-04-05 17:31:41 +0900

    test

--- kmod.c
+++ kmod.c
@@ -230,7 +230,7 @@ static int rohc_comp(struct rohc_init *rcouple, struct sk_buff *skb) {
 	uint8_t rohc_pkt_buf[skb->data_len];
 	struct rohc_buf rohc_packet = rohc_buf_init_empty(rohc_pkt_buf, skb->data_len);
 */
-	struct rohc_buf rohc_packet = rohc_buf_init_empty(skb->data, BUFFER_SIZE);
+	struct rohc_buf rohc_packet = rohc_buf_init_empty(skb->data, skb->data_len);
 
 	struct rohc_buf feedback_to_send = rohc_buf_init_empty(rcouple->feedback_to_send_buf, BUFFER_SIZE);
 
@@ -294,7 +294,7 @@ static int rohc_decomp(struct rohc_init *rcouple, struct sk_buff *skb) {
 	uint8_t decomp_buf[skb->data_len];
 	struct rohc_buf decomp_packet = rohc_buf_init_empty(decomp_buf, skb->data_len);
 */
-	struct rohc_buf decomp_packet = rohc_buf_init_empty(skb->data, BUFFER_SIZE);
+	struct rohc_buf decomp_packet = rohc_buf_init_empty(skb->data, skb->data_len);
 	uint8_t rcvd_feedback_buf[BUFFER_SIZE];
 	struct rohc_buf rcvd_feedback = rohc_buf_init_empty(rcvd_feedback_buf, BUFFER_SIZE);
 
