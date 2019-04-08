5341b326182004d02854e0cb891a9b6e093735b3
epicari <aiek261247@gmail.com>
2019-04-08 23:37:04 +0900

    test

--- kmod.c
+++ kmod.c
@@ -56,6 +56,7 @@ static struct rohc_init {
 
 struct sk_buff *r_skb;
 struct timespec unix_ts;
+struct rohc_ts arrival_time;
 
 static struct rohc_init rinit;
 
@@ -178,15 +179,15 @@ static int rohc_comp(struct rohc_init *rcouple, struct sk_buff *skb) {
 
 	r_skb = skb;
 	
-	struct rohc_ts arrival_time = {
+	arrival_time = {
 		.sec = unix_ts.tv_sec ,
 		.nsec = unix_ts.tv_nsec
 	};
 
 	struct rohc_buf ip_packet = rohc_buf_init_full(r_skb->data, r_skb->hdr_len, arrival_time);
 
-	uint8_t rohc_pkt_buf[r_skb->data_len];
-	struct rohc_buf rohc_packet = rohc_buf_init_empty(rohc_pkt_buf, r_skb->data_len);
+	uint8_t rohc_pkt_buf[r_skb->hdr_len];
+	struct rohc_buf rohc_packet = rohc_buf_init_empty(rohc_pkt_buf, r_skb->hdr_len);
 
 	//struct rohc_buf feedback_to_send = rohc_buf_init_empty(rcouple->feedback_to_send_buf, skb->data_len);
 /*
@@ -229,15 +230,15 @@ static int rohc_decomp(struct rohc_init *rcouple, struct sk_buff *skb) {
 
 	r_skb = skb;
 	
-	struct rohc_ts arrival_time = {
+	arrival_time = {
 		.sec = unix_ts.tv_sec ,
 		.nsec = unix_ts.tv_nsec
 	};
 
 	struct rohc_buf ip_packet = rohc_buf_init_full(r_skb->data, skb->hdr_len, arrival_time);
 	
-	uint8_t decomp_buf[r_skb->data_len];
-	struct rohc_buf decomp_packet = rohc_buf_init_empty(decomp_buf, r_skb->data_len);
+	uint8_t decomp_buf[r_skb->hdr_len];
+	struct rohc_buf decomp_packet = rohc_buf_init_empty(decomp_buf, r_skb->hdr_len);
 /*
 	uint8_t rcvd_feedback_buf[skb->data_len];
 	struct rohc_buf rcvd_feedback = rohc_buf_init_empty(rcvd_feedback_buf, skb->data_len);
