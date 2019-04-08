6acd412ab4e41489d8901422c6a2f186aa18ee5e
epicari <aiek261247@gmail.com>
2019-04-08 23:24:47 +0900

    test

--- kmod.c
+++ kmod.c
@@ -64,7 +64,7 @@ static struct rohc_init {
 
 };
 
-struct sk_buff *r_skb = skb;
+struct sk_buff *r_skb;
 static struct rohc_init rinit;
 
 static struct nf_hook_ops nfin;
@@ -185,6 +185,7 @@ static int rohc_comp(struct rohc_init *rcouple, struct sk_buff *skb) {
 	pr_info("ROHC_COMP\n");
 
 	//struct sk_buff *r_skb = skb;
+	r_skb = skb;
 
 	struct timespec unix_ts;
 	
@@ -238,6 +239,8 @@ static int rohc_decomp(struct rohc_init *rcouple, struct sk_buff *skb) {
 
 	pr_info("ROHC_DECOMP\n");
 
+	r_skb = skb;
+	
 	struct timespec unix_ts;
 	
 	const struct rohc_ts arrival_time = {
